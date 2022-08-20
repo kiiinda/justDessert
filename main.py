a30875ab9b62770eb826c95fc8713883f59b7a2b
oknyawira@gmail.com <oknyawira@gmail.com>
2022-08-21 02:08:10 +0300

    Instantiate class

---
+++ main.py
@@ -0,0 +1,18 @@
+from quiz import Question
+
+quizPrompts = [
+    "Welche Farbe hat eine Orange ?\n(a) rot\n(b) orange\n(c) gelb\n\n",
+    "Welche Farbe hat eine reife Banane?\n(a) gelb\n(b) grune\n (c) rosa\n\n",
+    "Welche Farbe hat eine Schokolade?\n(a)Schwarz \n(b)dunkelbraun \n\n"
+]
+questions = [
+    Question(quizPrompts[0], "b"),
+    Question(quizPrompts[1], "a"),
+    Question(quizPrompts[2], "b")
+]
+
+
+
+
+
+#test(questions)
